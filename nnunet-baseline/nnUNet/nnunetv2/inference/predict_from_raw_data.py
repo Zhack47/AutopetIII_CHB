425924e459aea3dc9da19df44a79f41bb6ee8512
zhack47 <mesbah.zacharia@sfr.fr>
2024-08-14 23:42:34 +0200

    Secret efficiency mechanic

--- nnunet-baseline/nnUNet/nnunetv2/inference/predict_from_raw_data.py
+++ nnunet-baseline/nnUNet/nnunetv2/inference/predict_from_raw_data.py
@@ -710,7 +710,7 @@ class nnUNetPredictor_efficient(nnUNetPredictor):
                                        num_threads_in_multithreaded=1, verbose=self.verbose)
         print(f"Processing mask")
         mask_plans = deepcopy(self.plans_manager)
-        mask_plans.plans["configurations"]["3d_fullres"]["resampling_fn_data_kwargs"]["order"]=0
+        mask_plans.plans["configurations"]["3d_fullres"]["resampling_fn_data_kwargs"]["order"]=1
         mask_plans.plans["configurations"]["3d_fullres"]["resampling_fn_data_kwargs"]["is_seg"]=True
         ppm = PreprocessAdapterFromNpy([mask], [segmentation_previous_stage], [image_properties],
                                        [output_file_truncated],
